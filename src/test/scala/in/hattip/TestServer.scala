cb424a3f1a36d579ea11df82d744879965ec7a36
Dhananjay Nene <dhananjay.nene@gmail.com>
2012-03-03 19:26:20 +0530

    Made tests stronger by intercepting and verifying requests received at the server

--- src/test/scala/in/hattip/TestServer.scala
+++ src/test/scala/in/hattip/TestServer.scala
@@ -21,23 +21,44 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import collection.JavaConversions._
 
-case class Req(val method: String, val uri: String)
+class RequestListener {
+  var lastRequest: Option[Req] = None
+  def send(req: Req) {
+    lastRequest = Some(req)
+  }
+  def clear() {
+    lastRequest = None
+  }
+  def get = lastRequest
+}
 
-class TestHandler extends AbstractHandler {
+case class Req(val method: String, val uri: String, headers: List[(String,String)], data: String = "")
+
+class TestHandler(l: RequestListener) extends AbstractHandler {
     def handle(target : String, baseReq : Request, request : HttpServletRequest, response : HttpServletResponse) {
       // set by default. Change later if required
       response.setContentType("text/html;charset=utf-8")
       response.setStatus(HttpServletResponse.SC_OK)
-      request.getHeaderNames() foreach { hdrname =>
-        println("===> Header:" + hdrname.toString() + "," + request.getHeader(hdrname))
-      }
-      val req = Req(baseReq.getMethod(), baseReq.getRequestURI())
+      val headers = request.getHeaderNames() map { hdrname =>
+        (hdrname, request.getHeader(hdrname))
+      } toList
+      var bytes = new Array[Byte](4096)
+      val count = request.getInputStream().read(bytes)
+      println("============================> ", count)
+      val req = if (count <= 0) 
+              Req(baseReq.getMethod(), baseReq.getRequestURI(),headers sorted)
+            else {
+              Req(baseReq.getMethod(), baseReq.getRequestURI(),headers sorted, new String(bytes.take(count)))
+            }
+              
+      l.send(req)
+      
       req match {
-        case Req("GET","/") =>
+        case Req("GET","/",_,_) =>
           response.getWriter().println("<html>Hello World!</html>")
-        case Req("GET","/index.html") =>
+        case Req("GET","/index.html",_,_) =>
           response.getWriter().println("<html>Hello World!</html>")
-        case Req("GET","/index.xml") =>
+        case Req("GET","/index.xml",_,_) =>
           response.setContentType("text/xml;charset=utf-8")
           response.getWriter().println("""
               <project name="hat.tip">
@@ -48,20 +69,11 @@ class TestHandler extends AbstractHandler {
                   </dependencies>
               </project>
               """)
-        case Req("GET","/nonexistent.file") =>
+        case Req("GET","/nonexistent.file",_,_) =>
           response.setStatus(HttpServletResponse.SC_NOT_FOUND)
-        case Req("POST","/post.do") =>
-          var content = new ListBuffer[String]()
-          val reader = request.getReader()
-          var line = reader.readLine()
-          while (line != null)
-          {
-              line = reader.readLine()
-              content += line
-          }
-          val body = content.toList
-          response.getWriter().println(body)
-        case Req("GET","/secure/index.html") =>
+        case Req("POST","/post.do",_,_) =>
+          response.getWriter().println(req.data)
+        case Req("GET","/secure/index.html",_,_) =>
           response.getWriter().println("<html>Hello Secure World!</html>")
       }
       baseReq.setHandled(true)
@@ -71,8 +83,8 @@ class TestHandler extends AbstractHandler {
 object TestServer {
     val server = new Server(8088)
 
-    def start() {
-      server.setHandler(new TestHandler())
+    def start(l: RequestListener) {
+      server.setHandler(new TestHandler(l))
       server.start()
     }
     def stop() {
@@ -84,36 +96,35 @@ object TestServer {
 object SecureServer {
     // have trouble installing multiple handlers on the same server
     // hence currently starting two servers
-    val server2 = new Server(9088)
-    def start() {
-      server2.setHandler(getSecureHandler())
-      server2.start()
-    }
-    def stop() {
-      server2.stop()
-    }
+  val server2 = new Server(9088)
+  def start(l: RequestListener) {
+    server2.setHandler(getSecureHandler(l: RequestListener))
+    server2.start()
+  }
+  def stop() {
+    server2.stop()
+  }
 
-    def getSecureHandler(): Handler = {
-
-    val constraint = new Constraint()
-    constraint setName Constraint.__BASIC_AUTH
-    constraint setRoles Array[String]("user","admin","moderator")
-    constraint setAuthenticate true
-
-    val cm = new ConstraintMapping()
-    cm.setConstraint(constraint);
-    cm.setPathSpec("/secure/*");
-
-    val sh = new ConstraintSecurityHandler()
-    sh.setConstraintMappings(Array[ConstraintMapping](cm));
-    sh.setHandler(new TestHandler())
-    sh.setRealmName("realm")
-      val realmPropFile = MavenTestingUtils.getTestResourceFile("realm.properties");
-      val loginService = new HashLoginService("realm",realmPropFile.getAbsolutePath());
-      sh.setLoginService(loginService);
-      sh.setAuthenticator(new BasicAuthenticator());
-
-    sh
+  def getSecureHandler(l: RequestListener): Handler = {
+  val constraint = new Constraint()
+  constraint setName Constraint.__BASIC_AUTH
+  constraint setRoles Array[String]("user","admin","moderator")
+  constraint setAuthenticate true
+  
+  val cm = new ConstraintMapping()
+  cm.setConstraint(constraint);
+  cm.setPathSpec("/secure/*");
+  
+  val sh = new ConstraintSecurityHandler()
+  sh.setConstraintMappings(Array[ConstraintMapping](cm));
+  sh.setHandler(new TestHandler(l))
+  sh.setRealmName("realm")
+  
+  val realmPropFile = MavenTestingUtils.getTestResourceFile("realm.properties");
+  val loginService = new HashLoginService("realm",realmPropFile.getAbsolutePath());
+  sh.setLoginService(loginService);
+  sh.setAuthenticator(new BasicAuthenticator());
+  sh
   }
 }
 
--- src/test/scala/in/hattip/test.scala
+++ src/test/scala/in/hattip/test.scala
@@ -12,9 +12,10 @@ import in.hattip.Hattip.str2HttpEndpoint
 @RunWith(classOf[JUnitRunner])
 class TestHattip extends SpecificationWithJUnit with BeforeExample with AfterExample {
   sequential
+  val listener = new RequestListener()
   def before = {
-    TestServer.start()
-    SecureServer.start()
+    TestServer.start(listener)
+    SecureServer.start(listener)
     WebsocketServer.start()
   }
   def after = {
@@ -37,28 +38,39 @@ class TestHattip extends SpecificationWithJUnit with BeforeExample with AfterExa
 
   "The hattip client library" should {
     "fetch a page successfully" in {
+      listener.clear()
       val resp = host get;
       resp.code must_== 200
       resp.str must contain("<html>Hello World!</html>")
+      listener.get must_== Some(Req("GET","/",
+            List[(String,String)](("Host","localhost:8088"))))
     }
 
     "be able to pass headers successfully" in {
+      listener.clear()
       val resp = host withHeaders(
           "foo" -> "bar", 
-          "baz"->"jaz",
-          "Content-type" -> "application/xml",
-          "Accept"->"application/xml") get;
+          "baz"->"jaz") get;
 
       resp.code must_== 200
       resp.str must contain("<html>Hello World!</html>")
+      listener.get must_== Some(Req("GET","/",
+            List[(String,String)](
+                ("Host","localhost:8088"), 
+                ("foo","bar"),
+                ("baz","jaz")) sorted))
     }
 
     "detect a 404 or other http codes appropriately" in {
+      listener.clear()
       val resp = host / "nonexistent.file" get;
       resp.code must_== 404
+      listener.get must_== Some(Req("GET","/nonexistent.file",
+            List[(String,String)](("Host","localhost:8088"))))
     }
 
     "skip the normal handler block in case of non 200, yet reach the error handler" in {
+      listener.clear()
       var enteredSuccessBlock = false;
       var capturedInErrorBlock = false;
       val resp = host / "nonexistent.file" get {
@@ -69,52 +81,78 @@ class TestHattip extends SpecificationWithJUnit with BeforeExample with AfterExa
       }
       enteredSuccessBlock must_==(false)
       capturedInErrorBlock must_==(true)
-    }
+      listener.get must_== Some(Req("GET","/nonexistent.file",
+                                    List[(String,String)](("Host","localhost:8088"))))
+   }
 
     "post data successfully" in {
-      val resp = host / "post.do" post("""This is
+      listener.clear()
+      val longString = """This is
             very very long
-              a long string""");
+              a long string"""
+      val resp = host / "post.do" post(longString);
       resp.code must_== 200
       resp.str must contain("very very long")
-    }
+      val data = listener.get.get.data
+      println("==============>" + data.length)
+      listener.get must_== Some(Req("POST","/post.do",List[(String,String)](
+          ("Host","localhost:8088"),
+          ("Content-Length", longString.length().toString()),
+          ("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
+          ) sorted,longString))
+   }
 
     "parse data as xml" in {
+      listener.clear()
       val xml = (host / "index.xml" get) asXml;
       val dependencies = xml\\"dependencies"\"dependency"
       dependencies.length must_==(3)
+      listener.get must_== Some(Req("GET","/index.xml",
+                                    List[(String,String)](("Host","localhost:8088"))))
     }
 
     "parse data as anti-xml" in {
+      listener.clear()
       val xml = (host / "index.xml" get) asAntiXml;
       val dependencies = xml\\"dependencies"\"dependency"
       dependencies.length must_==(3)
+      listener.get must_== Some(Req("GET","/index.xml",
+                                    List[(String,String)](("Host","localhost:8088"))))
     }
 
     "be able to detect 401 against secure resources" in {
+      listener.clear()
       var authErrorDetected = false;
       host2 / "secure" / "index.html" get { resp => ;} onErrorCode {
         case(401) => authErrorDetected = true
       }
 
       authErrorDetected must_==(true)
+      listener.get must_== None
     }
 
     "be able perform basic authentication" in {
+      listener.clear()
       var authenticated = false;
       host2 / "secure" / "index.html" secure("realm", "jetty", "jetty") get { resp =>
         authenticated = true
       }
 
       authenticated must_==(true)
+      listener.get must_== Some(Req("GET","/secure/index.html",
+                                    List[(String,String)](
+                                        ("Host","localhost:9088"),
+                                        ("Authorization","Basic amV0dHk6amV0dHk=")) sorted))
     }
     
     "be able to send a websocket text message" in {
+      listener.clear()
       val conn = wsHost open "in.hattip.hattip";
       conn ! "message .. message .. message"
       true must_==(true)
     }
     "be able to read a websocket text message" in {
+      listener.clear()
       val conn = wsHost open "in.hattip.hattip";
       var receivedPong = false
       conn onMessage { msg =>
